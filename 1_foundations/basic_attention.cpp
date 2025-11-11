b4503b086e11ab37a023619748fd8cfc9ff51bb7
Saber <mohamed.saber@edu.uiz.ac.ma>
2025-11-11 17:22:17 +0100

    feat: basic attention mechanism

---
+++ 1_foundations/basic_attention.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip> // For std::setprecision
+
+// SoftMax function, takes a vector and returns the SoftMax probabilities
+std::vector < double > softmax(const std::vector < double > & input) {
+  // declare output vector
+  std::vector < double > output(input.size());
+
+  // compute max value for numerical stability
+  double max_val = * std::max_element(input.begin(), input.end());
+  double sum = 0.0;
+
+  for (size_t i = 0; i < input.size(); i++) {
+    output[i] = std::exp(input[i] - max_val);
+    sum += output[i];
+  }
+
+  for (size_t i = 0; i < output.size(); ++i) {
+    output[i] /= sum;
+  }
+  return output;
+}
+
+// dot product Function
+double dot_product(const std::vector < double > & a,
+  const std::vector < double > & b) {
+  double result = 0.0;
+  for (size_t i = 0; i < a.size(); ++i) {
+    result += a[i] * b[i];
+  }
+  return result;
+}
+
+// core attention function
+std::vector < double > attention(const std::vector < double > & query,
+  const std::vector < std::vector < double > > & keys,
+    const std::vector < std::vector < double > > & values) {
+
+  // calculate attention scores
+  std::vector < double > scores;
+
+  for (size_t i = 0; i < keys.size(); ++i) {
+    scores.push_back(dot_product(query, keys[i]));
+  }
+  // apply softmax to scores
+  std::vector < double > weights = softmax(scores);
+
+  // compute weighted sum of values using weights
+  std::vector < double > output(values[0].size(), 0.0); // initialize output vector with zeros
+  for (size_t i = 0; i < values.size(); ++i) {
+    for (size_t j = 0; j < values[i].size(); ++j) {
+      output[j] += weights[i] * values[i][j];
+    }
+  }
+  return output;
+}
+
+// main function
+int main() {
+  // Query, Value, and Key matrices are fundamental components of the attention mechanism in transformer models.
+  // test data
+  std::vector < double > query = {
+    1.0,
+    0.0,
+    1.0
+  };
+  std::vector < std::vector < double >> keys = {
+    {
+      1.0,
+      0.0,
+      0.0
+    },
+    {
+      0.0,
+      1.0,
+      0.0
+    },
+    {
+      1.0,
+      0.0,
+      1.0
+    }
+  };
+  std::vector < std::vector < double >> values = {
+    {
+      1.0,
+      0.0
+    },
+    {
+      10.0,
+      0.0
+    },
+    {
+      100.0,
+      5.0
+    }
+  };
+
+  // compute attention output
+  std::vector < double > output = attention(query, keys, values);
+  // print output
+  std::cout << "Attention Output: ";
+  for (const auto & val: output) {
+    std::cout << std::setprecision(4) << val << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
